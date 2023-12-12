73ed57804a926f1e87c8bb4819f68c5584a47948
Ed Brown <edb56@live.co.uk>
2023-12-12 16:35:48 +0000

    first api call with buttons

--- app/pages/1Current_market.py
+++ app/pages/1Current_market.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas_ta as pta
 import os
 import plotly.graph_objects as go
+import plotly.express as px
+
 
 
 st.markdown('<h1 style="color:#FFB81C;text-align: center;font-size: 72px;">Bitcoin Cryptocurrency Chart</h1>', unsafe_allow_html=True)
@@ -23,9 +25,9 @@ st.markdown('<h5 style="color:#CCCCCC;text-align: left;">\
 st.markdown('<p style="color:#CCCCCC;text-align: left;">\
     If you would like to view additional technical indicators overlaid on the bitcoin graph please select from the below.</p>', unsafe_allow_html=True)
 
-rsi_upper_BB = st.checkbox("Show Upper Bollinger Band", value=True)
-rsi_middle_BB = st.checkbox("Show Middle Bollinger Band", value=True)
-rsi_lower_BB = st.checkbox("Show Lower Bollinger Band", value=True)
+rsi_upper_BB = st.checkbox("Show Upper Bollinger Band", value=False)
+rsi_middle_BB = st.checkbox("Show Middle Bollinger Band", value=False)
+rsi_lower_BB = st.checkbox("Show Lower Bollinger Band", value=False)
 
 # Calculate Bollinger Bands
 bbands_df = pta.bbands(df['close'], length=20)
@@ -82,6 +84,24 @@ fig.update_layout(
 
 # Display the candlestick chart using st.plotly_chart
 st.plotly_chart(fig)
+
+route_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+df_FAGI = pd.read_csv(os.path.join(route_path, 'raw_data/FearAndGreedIndex.csv'))
+
+df_FAGI['date'] = pd.to_datetime(df_FAGI['date'])
+
+fig_FAGI = px.line(df_FAGI, x='date', y='FAGI_score', title='FAGI Sentiment Over Time')
+
+fig_FAGI.update_layout(
+    title='Fear and Greed Chart',
+    xaxis_title='Date',
+    yaxis_title='Fear and Greed Score',
+    height=600,  # Adjust the height
+    width=1200,
+)
+
+st.plotly_chart(fig_FAGI)
 
 df['time'] = pd.to_datetime(df['time']).dt.strftime("%Y-%m-%d")
 
--- app/pages/2Crypto_predictor.py
+++ app/pages/2Crypto_predictor.py
@@ -3,9 +3,107 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import requests
+import os
+import plotly.express as px
 
-url= f'https://crypto-docker-image-gxpggkqnmq-uc.a.run.app/predict'
-r = requests.get(url)
-data = r.json()
+st.markdown('<h1 style="color:#FFB81C;text-align: center;font-size: 72px;">Crypto Predictor</h1>', unsafe_allow_html=True)
 
-st.write(data)
+st.markdown('<p style="color:#CCCCCC;text-align: left;">\
+    The graph below shows how the current model predicts against the actual values of bitcoin for the last 5 days.</p>', unsafe_allow_html=True)
+
+route_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+df = pd.read_csv(os.path.join(route_path, 'raw_data/BTC.csv'))
+
+df['time'] = pd.to_datetime(df['time'])
+
+df = df[['time','close']]
+
+tail_df = df.tail(7)
+
+st.markdown('<p style="color:#CCCCCC;text-align: left;">\
+    Please press the button below to display your crypto AI assistants predicted bitcoin price for the past five days!</p>', unsafe_allow_html=True)
+last_predictions_pressed = st.button('Check last Predictions')
+st.markdown('<p style="color:#CCCCCC;text-align: left;">\
+    Please press the button below to get your crypto AI assistants predicted bitcoin price for the next five days!</p>', unsafe_allow_html=True)
+future_predictions_pressed = st.button('Check future Predictions')
+
+if future_predictions_pressed:
+    # Example API URL, replace it with your actual API endpoint
+    api_url = 'https://crypto-docker-image-gxpggkqnmq-uc.a.run.app/predict'
+
+    # Fetch data from the API
+    response = requests.get(api_url)
+    print(response.status_code)
+
+    # Parse API response (assuming it's a JSON response)
+    api_data = response.json()
+
+    # Assuming the API returns a DataFrame-like structure with 'time' and 'close' columns
+    pred_last = np.array(api_data['predicted_price_last_7_days'])
+
+    #Extracting the dates
+    existing_dates = tail_df['time']
+
+    # Assuming the API returns a DataFrame-like structure with 'time' and 'close' columns
+    pred_future = np.array(api_data['predicted_price_for_next_7_days'])
+
+    # Creating future dates
+    future_dates = pd.date_range(existing_dates.max() + pd.Timedelta(days=1), periods=len(pred_future), freq='D')
+
+    api_df_exist = pd.DataFrame(data={'time': existing_dates,'pred': pred_last.flatten()})
+
+    api_df_future = pd.DataFrame(data={'time': future_dates, 'pred_future': pred_future.flatten()})
+
+    combined_df = pd.concat([tail_df, api_df_exist, api_df_future], ignore_index=True)
+
+    # Plotly Express line chart with dots
+    fig = px.line(combined_df, x='time', y=['close','pred','pred_future'], markers=True, line_shape='linear', title='Price Over Time')
+    fig.update_traces(mode='markers+lines', hovertemplate='%{y:.2f}', marker=dict(size=10))
+
+    # Streamlit display
+    st.plotly_chart(fig, use_container_width=True)
+elif last_predictions_pressed:
+    # Example API URL, replace it with your actual API endpoint
+    api_url = 'https://crypto-docker-image-gxpggkqnmq-uc.a.run.app/predict'
+
+    # Fetch data from the API
+    response = requests.get(api_url)
+    print(response.status_code)
+
+    # Parse API response (assuming it's a JSON response)
+    api_data = response.json()
+
+    # Assuming the API returns a DataFrame-like structure with 'time' and 'close' columns
+    pred_last = np.array(api_data['predicted_price_last_7_days'])
+
+    #Extracting the dates
+    existing_dates = tail_df['time']
+
+    api_df = pd.DataFrame(data={'time': existing_dates,'pred': pred_last.flatten()})
+
+    combined_df = pd.concat([tail_df, api_df], ignore_index=True)
+
+    # Plotly Express line chart with dots
+    fig = px.line(combined_df, x='time', y=['close', 'pred'], markers=True, line_shape='linear', title='Price Over Time')
+    fig.update_traces(mode='markers+lines', hovertemplate='%{y:.2f}', marker=dict(size=10))
+
+    # Streamlit display
+    st.plotly_chart(fig, use_container_width=True)
+else:
+    fig = px.line(tail_df, x='time', y='close', markers=True, line_shape='linear', title='Price Over Time')
+    fig.update_traces(mode='markers+lines', hovertemplate='%{y:.2f}', marker=dict(size=10))
+    st.plotly_chart(fig, use_container_width=True)
+
+if st.button('Test'):
+
+    api_url = 'https://crypto-docker-image-gxpggkqnmq-uc.a.run.app/predict'
+
+    # Fetch data from the API
+    response = requests.get(api_url)
+    print(response.status_code)
+
+    # Parse API response (assuming it's a JSON response)
+    api_data = response.json()
+
+    st.write(api_data)
